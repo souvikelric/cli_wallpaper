88fee4fcf726df4dca7c41a0b53e984459c830fe
Souvik <souvikelric@gmail.com>
2026-09-20 16:05:04 +0530

    cleanup

---
+++ ansi_constants.js
@@ -0,0 +1,18 @@
+const ansiColors = [
+  { clock: "\x1b[31m", background: "\x1b[101m" },
+  { clock: "\x1b[32m", background: "\x1b[102m" },
+  { clock: "\x1b[33m", background: "\x1b[103m" },
+  { clock: "\x1b[34m", background: "\x1b[104m" },
+  { clock: "\x1b[35m", background: "\x1b[105m" },
+  { clock: "\x1b[36m", background: "\x1b[106m" },
+  { clock: "\x1b[37m", background: "\x1b[107m" },
+  { clock: "\x1b[91m", background: "\x1b[101m" },
+  { clock: "\x1b[92m", background: "\x1b[102m" },
+  { clock: "\x1b[93m", background: "\x1b[103m" },
+  { clock: "\x1b[94m", background: "\x1b[104m" },
+  { clock: "\x1b[95m", background: "\x1b[105m" },
+  { clock: "\x1b[96m", background: "\x1b[106m" },
+  { clock: "\x1b[97m", background: "\x1b[107m" },
+];
+
+module.exports = { ansiColors };
--- terminal_basics.js
+++ terminal_basics.js
@@ -1,15 +1,5 @@
 const { renderDigit } = require("./digits");
-
-const drawCenteredText = (lines, rows, cols) => {
-  const paddingTop = Math.floor((rows - lines.length) / 2);
-
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    const padding = Math.floor((cols - line.length) / 2);
-
-    process.stdout.write(`\x1b[${paddingTop + i + 1};${padding + 1}H${line}`);
-  }
-};
+const { ansiColors } = require("./ansi_constants");
 
 const bottomStatusBar = (text) => {
   const row = process.stdout.rows;
@@ -34,23 +24,6 @@ process.stdout.write("\x1b[?7l"); // Disable scrolling
 let timer;
 let clockTopRow;
 let clockColorIndex = 0;
-
-const ansiColors = [
-  { clock: "\x1b[31m", background: "\x1b[101m" },
-  { clock: "\x1b[32m", background: "\x1b[102m" },
-  { clock: "\x1b[33m", background: "\x1b[103m" },
-  { clock: "\x1b[34m", background: "\x1b[104m" },
-  { clock: "\x1b[35m", background: "\x1b[105m" },
-  { clock: "\x1b[36m", background: "\x1b[106m" },
-  { clock: "\x1b[37m", background: "\x1b[107m" },
-  { clock: "\x1b[91m", background: "\x1b[101m" },
-  { clock: "\x1b[92m", background: "\x1b[102m" },
-  { clock: "\x1b[93m", background: "\x1b[103m" },
-  { clock: "\x1b[94m", background: "\x1b[104m" },
-  { clock: "\x1b[95m", background: "\x1b[105m" },
-  { clock: "\x1b[96m", background: "\x1b[106m" },
-  { clock: "\x1b[97m", background: "\x1b[107m" },
-];
 
 const formatTime = () => {
   const now = new Date();
